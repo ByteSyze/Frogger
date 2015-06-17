f7270bf65db818895fc5768dac1a7d0f95dd468c
unknown-user <unknown-user@fe80::a564:b0f4:4467:a7b1%11>
2015-06-17 01:39:56 -0700

    Add collider to test enemy

--- src/Unity/Assets/Scripts/EnemyLogic.cs
+++ src/Unity/Assets/Scripts/EnemyLogic.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class EnemyLogic : MonoBehaviour {
 	
 	GameObject enemy;
+	Rigidbody enemyRigidBody;
+
 	GameObject player;
 	
 	// Use this for initialization
 	void Start () {
 		enemy = GameObject.FindWithTag ("Enemy");
+		enemyRigidBody = GetComponent <Rigidbody> ();
+
 		player = GameObject.FindWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		enemy.transform.LookAt (player.transform);
-		enemy.transform.position += enemy.transform.forward * .05f;
+		enemyRigidBody.transform.position += enemy.transform.forward * .05f;
 	}
 }
--- src/Unity/Assets/Scripts/PlayerMovement.cs
+++ src/Unity/Assets/Scripts/PlayerMovement.cs
@@ -58,27 +58,7 @@ public class PlayerMovement : MonoBehaviour
 	
 	void Turning ()
 	{
-		// Create a ray from the mouse cursor on screen in the direction of the camera.
-		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-		// Create a RaycastHit variable to store information about what was hit by the ray.
-		RaycastHit floorHit;
-		
-		// Perform the raycast and if it hits something on the floor layer...
-		if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
-		{
-			// Create a vector from the player to the point on the floor the raycast from the mouse hit.
-			Vector3 playerToMouse = floorHit.point - transform.position;
-			
-			// Ensure the vector is entirely along the floor plane.
-			playerToMouse.y = 0f;
-			
-			// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
-			newRotation.y += 90;
-			// Set the player's rotation to this new rotation.
-			playerRigidbody.MoveRotation (newRotation);
-		}
+
 	}
 	
 	void Animating (float h, float v)
