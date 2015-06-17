258577875697efca61610d90c13531fac10d4d7d
Insomniac10102 <ins10102@gmail.com>
2015-06-16 23:57:14 -0700

    Initial shift to Unity

---
+++ src/Unity/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+	Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+
+		anim["idle"].wrapMode = WrapMode.Loop;
+		anim ["leap"].wrapMode = WrapMode.Once;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("w"))
+			anim.CrossFade("leap", 0.2f);
+	}
+}
---
+++ src/Unity/Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLogic : MonoBehaviour {
+	
+	GameObject enemy;
+	GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.FindWithTag ("Enemy");
+		player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		enemy.transform.LookAt (player.transform);
+		enemy.transform.position += enemy.transform.forward;
+	}
+}
